72a83deeac5eb2ab4b034fdba5ebc2e6e2ba5b19
Łukasz Berliński <47150002+dvdluke@users.noreply.github.com>
2021-03-11 13:59:34 +0100

    Update siema.java

--- src/siema.java
+++ src/siema.java
@@ -1,5 +1,5 @@
 public class siema {
     public static void main(String[] args) {
-        System.out.println("siema łuki jestes kox totalny");
+        System.out.println("siema ale z was giga koxy nie moge normalnie");
     }
 }
