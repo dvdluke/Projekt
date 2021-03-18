69c695b1951cce5fb1b7ef790e3447f6149ad761
xardesucha <79457563+xardesucha@users.noreply.github.com>
2021-03-18 16:12:29 +0100

    update

--- src/Slots.java
+++ src/Slots.java
@@ -13,34 +13,123 @@ public class Slots {
         double reward = 0;
         double bet = 0;
         try {
-        do {
-            System.out.println("Witamy w Automacie! 1");
-            System.out.println("Gra polega na wylosowaniu 3 tych samych liczb. Im większe liczby tym większa nagroda. Liczby są od 0 do 6 w 5 rzędach..");
-            System.out.println("Twoje saldo wynosi: $" + player.getMoney());
-            System.out.println("Jaką kwotę chcesz postawić?");
             do {
+                System.out.println("Witamy w Automacie! 1");
+                System.out.println("Gra polega na wylosowaniu 3 tych samych liczb. Im większe liczby tym większa nagroda. Liczby są od 0 do 6.");
+                System.out.println("Twoje saldo wynosi: $" + player.getMoney());
+                do {
 
-                System.out.println("Jaką kwotę chcesz postawić?");
-                bet = scanner.nextDouble();
-                if (bet > 0) {
-                    if (player.getMoney() < bet) System.out.println("Nie masz tyle pieniędzy!");
-                    else {
-                        player.setMoney(player.getMoney() - bet);
+                    System.out.println("Jaką kwotę chcesz postawić?");
+                    bet = scanner.nextDouble();
+                    if (bet > 0) {
+                        if (player.getMoney() < bet) System.out.println("Nie masz tyle pieniędzy!");
+                        else {
+                            player.setMoney(player.getMoney() - bet);
+                        }
+                    } else if (bet < 0) {
+                        System.out.println("Podałeś złe dane.");
                     }
-                } else if (bet < 0) {
-                    System.out.println("Podałeś złe dane.");
+                }while (betAgain);
+                System.out.println("Twoje saldo bo obstawieniu = $" + player.getMoney());
+                System.out.println("Automat losuje twoje liczby...");
+                TimeUnit.SECONDS.sleep(3);
+                int a = random.nextInt(7);
+                int b = random.nextInt(7);
+                int c = random.nextInt(7);
+                switch (a)
+                {
+                    case 0:
+                        System.out.println("7");
+                        break;
+                    case 1:
+                        System.out.println("$");
+                        break;
+                    case 2:
+                        System.out.println("A");
+                        break;
+                    case 3:
+                        System.out.println("B");
+                        break;
+                    case 4:
+                        System.out.println("C");
+                        break;
+                    default:
+                        System.out.println("D");
                 }
-            }while (betAgain);
-            System.out.println("Twoje saldo bo obstawieniu = $" + player.getMoney());
-            System.out.println("Automat losuje twoje liczby...");
-            TimeUnit.SECONDS.sleep(3);
-            int liczba1 = random.nextInt(7);
-            int liczba2 = random.nextInt(7);
-            int liczba3 = random.nextInt(7);
+
+                switch (b)
+                {
+                    case 0:
+                        System.out.println("7");
+                        break;
+                    case 1:
+                        System.out.println("$");
+                        break;
+                    case 2:
+                        System.out.println("A");
+                        break;
+                    case 3:
+                        System.out.println("B");
+                        break;
+                    case 4:
+                        System.out.println("C");
+                        break;
+                    default:
+                        System.out.println("D");
+                }
+
+                switch (c)
+                {
+                    case 0:
+                        System.out.println("7");
+                        break;
+                    case 1:
+                        System.out.println("$");
+                        break;
+                    case 2:
+                        System.out.println("A");
+                        break;
+                    case 3:
+                        System.out.println("B");
+                        break;
+                    case 4:
+                        System.out.println("C");
+                        break;
+                    default:
+                        System.out.println("D");
+                }
+                if (a != b && a != c && b != c)
+                {
+                    System.out.println("Niestety nic nie wygrałeś.");
+                }
+                else if (a == b || a == c || b == c)
+                {
+
+                    System.out.println("Gratuluję, wygrałeś $" + reward);
+                    player.setMoney(player.getMoney() + reward); ;
+                }
+                else if (a == b && a == c && a != 0)
+                {
+
+                    System.out.println("Gratuluję, wygrałeś $" + reward );
+                    player.setMoney(player.getMoney() + reward);
+                }
+                else if (a == 0 && b == 0 && c == 0)
+                {
+
+                    System.out.println("Gratuluję wygrałeś główną nagrodę $" + reward);
+
+                }
+
+                System.out.println("Continue? y/n ");
+                scanner.next();
 
 
 
-        }while(play);
+
+
+
+            }while(play);
         } catch (Exception exception) {
             exception.printStackTrace();
         }
