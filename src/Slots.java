85cef98c41acbda2592afad166a6295d28d7b0bf
Łukasz Berliński <47150002+dvdluke@users.noreply.github.com>
2021-03-18 21:40:10 +0100

    Final Update of Project

--- src/Slots.java
+++ src/Slots.java
@@ -177,12 +177,8 @@ public class Slots {
                 }
                 if (pierwsza != druga && pierwsza != trzecia && druga != trzecia)
                 {
-<<<<<<< Updated upstream
-                    System.out.println("\nNiestety nic nie wygrałeś.");
-                    break;
-=======
                     System.out.println("\nNiestety nic nie wygrałeś. ;(");
->>>>>>> Stashed changes
+                    break;
                 }
                 else if (pierwsza == druga || pierwsza == trzecia || druga == trzecia)
                 {
@@ -208,11 +204,7 @@ public class Slots {
                 }
                     do{ if (!playAgain && player.getMoney() > 0) {
                         System.out.println("Twój aktualny balans to $" + player.getMoney());
-<<<<<<< Updated upstream
-                        System.out.println("Chcesz znowu zagrać? tak [y] nie [n]");
-=======
                         System.out.println("Czy chciałbyś znowu zagrać? Tak [y] Nie [n]");
->>>>>>> Stashed changes
                         String draw = scanner.next();
                         if (draw.equals("n") || draw.equals("N")) {
                             play = false;
